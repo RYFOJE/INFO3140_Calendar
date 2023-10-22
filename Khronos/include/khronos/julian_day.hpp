e556b6a840f99729a4eae69e61ec6b5d37ee3d68
ryfoje <ryfoje2@gmail.com>
2023-10-22 16:54:01 -0400

    Moved code definition for functions into correct files for julian_day.hpp

--- Khronos/include/khronos/julian_day.hpp
+++ Khronos/include/khronos/julian_day.hpp
@@ -8,7 +8,7 @@ Revision History
 -------------------------------------------------------------
 
 Version 2021.09.25
-	Repaced relational operators with <=>
+	Replaced relational operators with <=>
 
 Version 2016.09.30
 	Added: Phase List
@@ -52,52 +52,14 @@ namespace khronos {
 	
 	// FUNCTIONS
 
-	inline jd_t jd_to_jdn(jd_t jd) {
-		return floor(jd + 0.5);
-	}
+	jd_t jd_to_jdn(jd_t jd);
 
-	inline void jd_to_gregorian(jd_t jd, year_t& year, month_t& month, day_t& day) {
+	void jd_to_gregorian(jd_t jd, year_t& year, month_t& month, day_t& day);
 
-		// TOD Add better handling for Gregorian_Epoch
-		if (jd == GREGORIAN_EPOCH) {
-			year = 1;
-			month = 1;
-			day = 1;
-			return;
-		}
+	void jd_to_gregorian(jd_t jd, year_t& year, month_t& month, day_t& day,
+		hour_t& hours, minute_t& minutes, second_t& seconds);
 
-		jd = floor(jd - 0.5) + 0.5;
-		long long j = static_cast<long long>(jd + 0.5) + 32044;
-		long long g = j / 146097;
-		long long dg = j % 146097;
-		long long c = (dg / 36524 + 1) * 3 / 4;
-		long long dc = dg - c * 36524;
-		long long b = dc / 1461;
-		long long db = dc % 1461;
-		long long a = (db / 365 + 1) * 3 / 4;
-		long long da = db - a * 365;
-		long long y = g * 400 + c * 100 + b * 4 + a;
-		long long m = (da * 5 + 308) / 153 - 2;
-		year = y - 4800 + (m + 2) / 12;
-		month = (m + 2) % 12 + 1;
-		day = static_cast<day_t>(da - (m + 4) * 153 / 5 + 123);
-	}
-
-	inline void jd_to_gregorian(jd_t jd, year_t& year, month_t& month, day_t& day,
-								hour_t& hours, minute_t& minutes, second_t& seconds) {
-
-		jd_to_gregorian(jd, year, month, day);
-
-		// TODO Better implementation of the modulo
-		tod_to_hms(utility::mod(jd, 1), hours, minutes, seconds);
-
-	}
-
-	inline civil_weekday_codes_long day_of_week(const jd_t jd) {
-
-		return static_cast<civil_weekday_codes_long>(utility::mod((jd + 0.5), 7));
-
-	}
+	civil_weekday_codes_long day_of_week(const jd_t jd);
 	
 
 	/** JD CLASS DEFINITION **/
