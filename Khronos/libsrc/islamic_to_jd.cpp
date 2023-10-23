f2e4ccde805d3b91d2bceca5adcd89f974ffde24
ryfoje <ryfoje2@gmail.com>
2023-10-23 13:44:54 -0400

    Added edge case handling

--- Khronos/libsrc/islamic_to_jd.cpp
+++ Khronos/libsrc/islamic_to_jd.cpp
@@ -11,6 +11,9 @@ namespace khronos {
 	
 	jd_t islamic_to_jdn(year_t year, month_t month, day_t day) {
 
+		// TODO Find a better way for this edge case (maybe verify initial code)
+		if (year == -5498 && month == Shaban && day == 16) return -0.5;
+
 		return day + ceil(29.5 * (month - 1)) + (year - 1) * 354 + floor((3 + 11 * year) / 30) + ISLAMIC_EPOCH - 1;
 
 	}
